0a31bd34ce91ccb393237db9bbfa50d22a4553e2
Ujjwalkr983 <143831102+Ujjwalkr983@users.noreply.github.com>
2024-04-14 13:01:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -154,7 +154,7 @@ function App() {
                 target="_blank"
                 rel="noreferrer"
                 className="btn btn-outline-dark"
-                href="https://github.com/Vishesh-Pandey/v-music"
+                href="https://github.com/Ujjwalkr983/ujjwalmusicapp"
               >
                 <i className="bi bi-github mx-2"></i>Github
               </a>
